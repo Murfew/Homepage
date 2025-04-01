503354700cb5c2f7520edf4cd8fe3cad456eb486
Murfew <matthew.hurley953@gmail.com>
2025-04-01 14:15:05 -0400

    env setup

---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+// npm init -y
+
+// npm install --save-dev webpack webpack-cli style-loader css-loader
+// html-webpack-plugin webpack-dev-server webpack-merge html-loader babel-jest
+// @babel/core @babel/preset-env
+
+// import all files into js
+
+// npm run build to create a production build
+// npm start to host live server of development build
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: "body",
+    }),
+  ],
+};
